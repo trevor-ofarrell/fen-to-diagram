381e413442f03c603a1a31a13a90ee96617209ef
trevor-ofarrell <trevorofarrell963@gmail.com>
2021-06-24 02:17:31 -0700

    adding fen validation, and move indicator

--- client/src/Diagram.js
+++ client/src/Diagram.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Chessboard from "chessboardjsx";
+import Chess from "chess.js";
 
 const Diagram = () => {
   const [base64, setBase64] = useState('');
   const urlParams = new URLSearchParams(window.location.search);
-  const FEN = urlParams.get('fen').split(',');
   const [listening, setListening] = useState(false);
 
+  const FEN = urlParams.get('fen').split(',').filter(
+    fen => new Chess().load(fen) === true
+  ).map((item) => {
+    const chess = new Chess()
+    chess.load(item)
+    return [item, chess.turn()]
+  })
 
   useEffect(() => {
     if (!listening) {
@@ -15,14 +22,14 @@ const Diagram = () => {
         .then(response => {
           const reader = new FileReader();
           reader.readAsDataURL(response); 
-          reader.onloadend = function() {
+          reader.onloadend = () => {
               const base64data = reader.result;                
               setBase64(base64data)
           }
         })
       setListening(true);
     }
-  }, [base64, FEN, listening])
+  }, [base64, listening, FEN])
 
   return (
     <div className="bg-gray-400 min-h-screen h-full w-screen m-auto overflow-y-hidden">
@@ -30,8 +37,8 @@ const Diagram = () => {
         <div>
           <div className="max-w-lg m-auto grid grid-cols-2 pt-10 pl-6">
             <h2 className="text-3xl font-semibold text-center pt-6 mb-12">creating diagram</h2>
-            <div class=" flex justify-center items-center">
-              <div class="animate-spin rounded-full h-32 w-32 border-b-4 border-gray-900"></div>
+            <div className=" flex justify-center items-center">
+              <div className="animate-spin rounded-full h-32 w-32 border-b-4 border-gray-900"></div>
             </div>
           </div>
           <div
@@ -41,14 +48,17 @@ const Diagram = () => {
             {FEN.map((fen, i) => {
               return (
                 <div className="p-auto m-auto mb-16 h-auto">
+                  <div className="flex flex-row">
                     <span className="text-md font-normal text-black">#{i + 1}</span>
-                    <Chessboard
-                        id={`${i}key`}
-                        position={fen}
-                        transitionDuration={100}
-                        showNotation={false}
-                        width={350}
-                    />
+                    <div className={`rounded-full h-4 w-4 ${fen[1] === 'b' ? "bg-black" : "bg-white border-black border-1"} ml-auto mr-3`}></div>
+                  </div>
+                  <Chessboard
+                      id={`${i}key`}
+                      position={fen[0]}
+                      transitionDuration={100}
+                      showNotation={false}
+                      width={350}
+                  />
                 </div>
               )
             })}
