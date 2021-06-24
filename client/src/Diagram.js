6514ac05c97775d68d551a1dd793cf114085a520
trevor-ofarrell <trevorofarrell963@gmail.com>
2021-06-23 20:52:09 -0700

    adding progress spinner

--- client/src/Diagram.js
+++ client/src/Diagram.js
@@ -27,28 +27,36 @@ const Diagram = () => {
   return (
     <div className="bg-gray-400 h-screen w-screen m-auto">
       {!base64 ? (
-        <div
-          className={`grid ${FEN.length > 1 ? 'grid-cols-2' : 'grid-cols-1'} m-auto pt-12 h-auto`}
-          id="diagram"
-        >
-          {FEN.map((fen, i) => {
-            return (
-              <div className="p-auto m-auto mb-16 h-auto">
-                  <span className="text-md font-normal text-black">#{i + 1}</span>
-                  <Chessboard
-                      id={`${i}key`}
-                      position={fen}
-                      transitionDuration={100}
-                      showNotation={false}
-                      width={350}
-                  />
-              </div>
-            )
-          })}
+        <div>
+          <div className="grid grid-cols-2 pt-10">
+            <h2 className="text-3xl text-center pt-6 mb-12">creating diagram...</h2>
+            <div class=" flex justify-center items-center">
+              <div class="animate-spin rounded-full h-32 w-32 border-b-4 border-gray-900"></div>
+            </div>
+          </div>
+          <div
+            className={`grid ${FEN.length > 1 ? 'grid-cols-2' : 'grid-cols-1'} m-auto pt-12 h-auto`}
+            id="diagram"
+          >
+            {FEN.map((fen, i) => {
+              return (
+                <div className="p-auto m-auto mb-16 h-auto">
+                    <span className="text-md font-normal text-black">#{i + 1}</span>
+                    <Chessboard
+                        id={`${i}key`}
+                        position={fen}
+                        transitionDuration={100}
+                        showNotation={false}
+                        width={350}
+                    />
+                </div>
+              )
+            })}
+          </div>
         </div>
       ):(
           <>
-              <h2 className="text-3xl text-center pt-6 mb-12">PNG image of diagram</h2>
+              <h2 className="text-3xl text-center pt-6 mb-12">PNG of diagram created!</h2>
               <div className="m-auto">
                   <img
                     src={base64}
